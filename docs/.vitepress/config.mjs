4d9db02809962ed482fcb3925cc26ddfc84eacfb
Timmy <ccliu@net-chinese.com.tw>
2024-06-03 18:21:35 +0800

    ok

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base:'/repo/',
+  // base:'/repo/',
   title: "test",
   description: "a test",
   themeConfig: {
